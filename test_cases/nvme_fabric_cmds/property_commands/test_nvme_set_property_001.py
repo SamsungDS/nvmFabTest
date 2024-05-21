01354468b5177bff6ff18eefe11cb5bdcd3ddc51
Nihal Jahagirdar <nihal.j@partner.samsung.com>
2024-05-21 11:18:15 +0000

    remove abs path

--- test_cases/nvme_fabric_cmds/property_commands/test_nvme_set_property_001.py
+++ test_cases/nvme_fabric_cmds/property_commands/test_nvme_set_property_001.py
@@ -7,7 +7,6 @@ Verify that the shutdown completes.
 import ctypes
 import pytest,sys
 import time
-sys.path.insert(1, "/root/nihal223/nvmfabtest/")
 from lib.devlib.device_lib import Controller
 from lib.structlib.struct_admin_data_lib import IdentifyControllerData
 from test_cases.conftest import dummy
